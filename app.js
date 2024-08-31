960715e13dac90070c7034c6b0387b92e51a920f
feryardi70 <fery.ardiansyah94747@gmail.com>
2024-08-31 21:32:11 +0700

    Update app.js
    edit port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const expressLayout = require("express-ejs-layouts");
 const { loadContact, findContact, addContact, cekDuplikat } = require("./utils/contacts.js");
 const { body, validationResult, check } = require("express-validator");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 //gunakan template ejs
 app.set("view engine", "ejs");
