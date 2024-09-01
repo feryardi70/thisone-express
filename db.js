4805a38c37a5a1559cc0558c8c87c7f03b67d099
feryardi70 <fery.ardiansyah94747@gmail.com>
2024-09-01 19:16:38 +0700

    menambahkan file db.js

---
+++ db.js
@@ -0,0 +1,2 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://feryardiansyah94747:i4kCuJMR1I0G4F8H@cluster0.2dkoa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
