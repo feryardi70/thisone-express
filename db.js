c01871db6ceaa5ae966e53854971a12197196e5a
feryardi70 <fery.ardiansyah94747@gmail.com>
2024-09-05 22:01:24 +0700

    Update db.js
    remove slash

--- db.js
+++ db.js
@@ -1,2 +1,2 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://feryardiansyah94747:i4kCuJMR1I0G4F8H@cluster0.2dkoa.mongodb.net/test/?retryWrites=true&w=majority&appName=Cluster0");
+mongoose.connect("mongodb+srv://feryardiansyah94747:i4kCuJMR1I0G4F8H@cluster0.2dkoa.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0");
