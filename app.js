53b89c412459f5e98264a9c8284a2fd9475bf04b
feryardi70 <fery.ardiansyah94747@gmail.com>
2024-09-08 17:00:34 +0700

    Update app.js
    removeContact2

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.get("/", (req, res) => {
 
 app.get("/arrival", (req, res) => {
   //res.send("Hello World!");
-  res.render("arrival", { layout: "Layouts/none, contacts" });
+  res.render("arrival", { layout: "Layouts/none" });
 });
 
 app.get("/about", (req, res) => {
